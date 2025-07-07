5c3195fd10551daa223e608c02a1b9c76737c9d9
dvali <ndval21@freeuni.edu.ge>
2025-07-07 14:57:16 +0400

    finished ui for login page

--- app/src/main/java/com/ikhut/messengerapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ikhut/messengerapp/presentation/MainActivity.kt
@@ -1,20 +1,23 @@
-package com.ikhut.messengerapp.presentation
+    package com.ikhut.messengerapp.presentation
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.ikhut.messengerapp.R
-import com.ikhut.messengerapp.databinding.ActivityMainBinding
+    import android.os.Bundle
+    import androidx.appcompat.app.AppCompatActivity
+    import com.ikhut.messengerapp.databinding.ActivityMainBinding
+    import com.ikhut.messengerapp.presentation.authentification.LoginFragment
 
-class MainActivity : AppCompatActivity() {
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding!!
+    class MainActivity : AppCompatActivity() {
+        private var _binding: ActivityMainBinding? = null
+        private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            _binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            if (savedInstanceState == null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(binding.mainActivityFragmentContainerView.id, LoginFragment())
+                    .commit()
+            }
+        }
     }
-}
