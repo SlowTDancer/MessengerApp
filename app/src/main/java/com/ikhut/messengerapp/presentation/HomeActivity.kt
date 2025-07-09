73e893f03e738686bb8e0b19cd1dddb3c6d5f7ef
dvali <ndval21@freeuni.edu.ge>
2025-07-09 14:16:05 +0400

    added navbar icons

--- app/src/main/java/com/ikhut/messengerapp/presentation/HomeActivity.kt
+++ app/src/main/java/com/ikhut/messengerapp/presentation/HomeActivity.kt
@@ -2,18 +2,12 @@ package com.ikhut.messengerapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomappbar.BottomAppBar
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 import com.ikhut.messengerapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private var _binding: ActivityHomeBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var bottomNavigation: BottomNavigationView
-    private lateinit var bottomAppBar: BottomAppBar
-    private lateinit var floatingActionButton: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +15,7 @@ class HomeActivity : AppCompatActivity() {
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bottomNavigationView.background = null
     }
 
     override fun onDestroy() {
