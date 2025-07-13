d80f69658c3d9dbda1676601ca0e6c4ae563f65f
SlowTDancer <ikhut21@freeuni.edu.ge>
2025-07-13 19:33:01 +0400

    added settings page UI

--- app/src/main/java/com/ikhut/messengerapp/presentation/HomeActivity.kt
+++ app/src/main/java/com/ikhut/messengerapp/presentation/HomeActivity.kt
@@ -2,14 +2,15 @@ package com.ikhut.messengerapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import com.ikhut.messengerapp.R
 import com.ikhut.messengerapp.databinding.ActivityHomeBinding
-import com.ikhut.messengerapp.presentation.authentification.LoginFragment
 import com.ikhut.messengerapp.presentation.homeFragments.ConversationListFragment
+import com.ikhut.messengerapp.presentation.homeFragments.SettingsFragment
 
 class HomeActivity : AppCompatActivity() {
     private var _binding: ActivityHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var username: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,12 +18,37 @@ class HomeActivity : AppCompatActivity() {
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        username = intent.getStringExtra("username") ?: getSharedPreferences(
+            "user_prefs", MODE_PRIVATE
+        ).getString("username", "Unknown")!!
+
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
         binding.bottomNavigationView.background = null
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(binding.homeActivityFragmentContainerView.id, ConversationListFragment()).commit()
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction().replace(
+                        binding.homeActivityFragmentContainerView.id, ConversationListFragment()
+                    ).commit()
+                    true
+                }
+
+                R.id.nav_settings -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(binding.homeActivityFragmentContainerView.id, SettingsFragment())
+                        .commit()
+                    true
+                }
+
+                else -> false
+            }
         }
+
+        binding.bottomNavigationView.selectedItemId = R.id.nav_home
     }
 
     override fun onDestroy() {
